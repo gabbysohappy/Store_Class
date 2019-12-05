d3c6f4d5381edaed2cd52d52bd4f333862fe7a62
Gabriella Algecera <gabriellaalgecera@Brians-MacBook-Pro.local>
2019-12-04 20:30:00 -0700

    end of class

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+  email: 'admin@admin.com',
+  password: 'password',
+  name: 'admin',
+  wallet: 100.00,
+  shoes: true,
+  shirt: true,
+  age: 84
+)
